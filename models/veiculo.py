ce05d707e55c84bffee9fb1bd067da6e3c2cedc4
dudubarbaro <dudubarbaro>
2023-08-22 14:02:12 -0300

    fix: many2many add in Veiculos

--- models/veiculo.py
+++ models/veiculo.py
@@ -7,21 +7,18 @@ from uploader.models import Image
 
 class Veiculo(models.Model):
     id = models.BigAutoField(primary_key=True)
-    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculos")
+    cor = models.ForeignKey(
+        Cor, on_delete=models.PROTECT, related_name="veiculos"
+    )
     modelo = models.ForeignKey(
         Modelo, on_delete=models.PROTECT, related_name="veiculos"
     )
     ano = models.IntegerField(null=True, blank=True)
     descricao = models.CharField(max_length=255, null=True, blank=True)
-    preco = models.DecimalField(decimal_places=2, max_digits=5, null=True, blank=True)
-    imagem = models.ForeignKey(
-        Image,
-        related_name="+",
-        on_delete=models.CASCADE,
-        null=True,
-        blank=True,
-        default=None,
+    preco = models.DecimalField(
+        decimal_places=2, max_digits=5, null=True, blank=True
     )
+    imagem = models.ManyToManyField(Image)
 
     def __str__(self):
         return f"{self.ano} ({self.descricao})"
